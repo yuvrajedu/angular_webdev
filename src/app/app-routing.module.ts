58ec478c55824e3f142b27670aec5f144ee021bb
yuvrajedu <yuvrajsr.edu@gmail.com>
2023-09-10 18:47:09 +0530

    updated app component files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { TempFormComponent } from './temp-form/temp-form.component';
+import { ReactFormComponent } from './react-form/react-form.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'about-us', component: AboutUsComponent}
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'temp-form', component: TempFormComponent},
+  {path: 'react-form', component: ReactFormComponent},
+  {path: 'user-details', component: UserDetailsComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TempFormComponent } from './temp-form/temp-form.component';
+import { ReactFormComponent } from './react-form/react-form.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    TempFormComponent,
+    ReactFormComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule, // for template driven form
+    ReactiveFormsModule // for reactive form
   ],
   providers: [],
   bootstrap: [AppComponent]
